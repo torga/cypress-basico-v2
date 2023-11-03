16c5261fe3e31fcce2c4c2cd8499ae257c2cd91c
torga <torgaigor@gmai.com>
2023-11-03 02:48:05 -0300

    adicionando exercicios da aula 2

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,59 +8,66 @@
 /// <reference types="Cypress" />
 
 describe('Acesso ao site de teste', function () {
-    beforeEach(() => {
+    //aula 2    
+    this.beforeEach(() => {
         cy.visit('src/index.html')
     })
-
-    it('Verifica o título da aplicação', function () {
+    // aula 1
+    it('Verificar o título da aplicação se é igual ao Central de Atendimento', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it('Preenche os campos obrigatórios e envia o formulário', function () {
-        cy.get('#firstName').click().type('Igor').should('have.value', 'Igor')
-        cy.get('#lastName').click().type('Torga').should('have.value', 'Torga')
-        cy.get('#email').click().type('Torgaigor@gmail.com').should('have.value', 'Torgaigor@gmail.com')
-        cy.get('#phone').click().type('79998888893').should('have.value', '79998888893')
-        cy.get('#open-text-area').click().type('nada com nada').should('have.value', 'nada com nada')
+    // aula 2
+    it('Enviar formuário preenchido com sucesso', () => {
+        cy.get('#firstName').type('Igor').should('have.value','Igor')
+        cy.get('#email').type('Torgaigor@gmail.com').should('have.value','Torgaigor@gmail.com')
+        cy.get('#lastName').type('Torga').should('have.value','Torga')
+        cy.get('#phone').type('79998888893').should('have.value','79998888893')
+        cy.get('#phone-checkbox')
+        cy.get('#open-text-area').type('79998888893').should('have.value','79998888893')
+        cy.get('#phone-checkbox').click()
         cy.get('.button').click()
         cy.get('.success > strong').should('be.visible')
     })
-
-    it('Validar delay', function(){
-        const longText = 'texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto '
-        cy.get('#firstName').click().type('Igor').should('have.value', 'Igor')
-        cy.get('#lastName').click().type('Torga').should('have.value', 'Torga')
-        cy.get('#email').click().type('Torgaigor@gmail.com').should('have.value', 'Torgaigor@gmail.com')
-        cy.get('#phone').click().type('79998888893').should('have.value', '79998888893')
-        cy.get('#open-text-area').click().type(longText,{delay:0})
-        cy.get('.button').click()
-    })
-    it('Preenche os campos obrigatórios e envia o formulário com e-mail incorreto', function () {
-        cy.get('#firstName').click().type('Igor')
-        cy.get('#lastName').click().type('Torga')
-        cy.get('#email').click().type('Torgaigorgmail.com')
-        cy.get('#phone').click().type('79998888893')
-        cy.get('#open-text-area').click().type('nada com nada')
-        cy.get('.button').click()
-        cy.get('.error').should('be.visible')
-    })
-    it('Preenche campo com valor não numerico no telefone', function () {
-        cy.get('#phone')
-          .click()
-          .type('tetetesteteet')
-          .should('have.value','')
-    })
-    it.only('Preenche campo com valor não numerico no telefone', function () {
-        cy.get('#firstName').click().type('Igor')
-        cy.get('#lastName').click().type('Torga')
-        cy.get('#email').click().type('Torgaigorgmail.com')
+    // aula 2
+    it('Enviar formuário preenchido com sucesso com menos delay', () => {
+        const BIG_TEXTO = 'pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai pai'
+        cy.get('#firstName').type('Igor').should('have.value','Igor')
+        cy.get('#email').type('Torgaigor@gmail.com').should('have.value','Torgaigor@gmail.com')
+        cy.get('#lastName').type('Torga').should('have.value','Torga')
+        cy.get('#phone').type('79998888893').should('have.value','79998888893')
+        cy.get('#phone-checkbox')
+        cy.get('#open-text-area').type(BIG_TEXTO, {delay:0}).should('have.value',BIG_TEXTO)
         cy.get('#phone-checkbox').click()
-        cy.get('#open-text-area').click().type('nada com nada')
+        cy.get('.button').click()
+        cy.get('.success > strong').should('be.visible')
+    })
+    it('Preenche os campos obrigatórios e envia o formulário com e-mail incorreto', () => {
+        cy.get('#firstName').type('Igor').should('have.value','Igor')
+        cy.get('#email').type('Torgaigorgmail.com').should('have.value','Torgaigorgmail.com')
+        cy.get('#lastName').type('Torga').should('have.value','Torga')
+        cy.get('#phone').type('79998888893').should('have.value','79998888893')
+        cy.get('#open-text-area').type('nada com nada')
         cy.get('.button').click()
         cy.get('.error').should('be.visible')
+    })
+    it('Preenche e envia o formulario sem telefone e exibe mensagem de erro', () => {
+        cy.clock()
+        cy.get('#firstName').type('Igor').should('have.value','Igor')
+        cy.get('#email').type('Torgaigor@gmail.com').should('have.value','Torgaigor@gmail.com')
+        cy.get('#lastName').type('Torga').should('have.value','Torga')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('nada com nada')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+    })
+    it('Preenche campo name e valida limpeza de campo', () => {
+        cy.get('#firstName').type('Ig)
+        cy.clear()
+     
 
     })
-    it ('Preenche campo com valor ', function(){
+    it('Preenche campo com valor ', function () {
         cy.preencheCamposFormulario()
         cy.get('.sucess').should('be.visible')
     })
